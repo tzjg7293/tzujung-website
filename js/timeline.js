429398b77b37d2a766e13e10a208546a3f9e076c
tzjg7293 <tzjg7293@gmail.com>
2023-03-13 12:25:58 -0700

    timeline scrolling transition

--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,5 @@
-(function($) {
-  $.fn.timeline = function() {
+(function ($) {
+  $.fn.timeline = function () {
     var selectors = {
       id: $(this),
       item: $(this).find(".timeline-item"),
@@ -10,23 +10,38 @@
     // selectors.id.css("background-image", "url(" + selectors.item.first().find(selectors.img).attr("src") + ")");
 
     var itemLength = selectors.item.length;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       var max, min;
+      var screen_height = screen.height;
+      // var pos = ($(this).scrollTop() + screen_height / 2);
       var pos = $(this).scrollTop();
-      selectors.item.each(function(i) {
+      selectors.item.each(function (i) {
+        console.log( i + ": " + $( this ).text());
         min = $(this).offset().top;
         max = ($(this).height() + $(this).offset().top);
-        var that = $(this)
-        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+        var item_middle = min + (max - min) / 2;
+        // console.log("Curr item: " + i);
+        // console.log("itemlength: " + itemLength);
+        // console.log("Item Height: " + $(this).height());
+        // console.log("Screen height: " + screen_height);
+        // console.log("Pos: " + pos);
+        // console.log("Item middle: " + item_middle);
+        // console.log("Min: " + min);
+        // console.log("Max: " + max);
+        // console.log("midpoint: " + (item_middle + screen_height / 2));
+
+        // var that = $(this)
+
+        if (i == itemLength - 2 && (pos + screen_height/3) > item_middle) {
           selectors.item.removeClass(selectors.activeClass);
           selectors.id.css("background-image", "url(" + selectors.item.last().find(selectors.img).attr('src') + ")");
           selectors.item.last().addClass(selectors.activeClass)
-        } else if (pos <= max - 40 && pos >= min) {
-            selectors.id.css("background-image", "url(" + $(this).find(selectors.img).attr('src') + ")");
-            selectors.item.removeClass(selectors.activeClass);
-            $(this).addClass(selectors.activeClass);
-          }
-        
+        } else if ((pos + screen_height/3) <= max - 40 && (pos + screen_height/3) >= min) {
+          selectors.id.css("background-image", "url(" + $(this).find(selectors.img).attr('src') + ")");
+          selectors.item.removeClass(selectors.activeClass);
+          $(this).addClass(selectors.activeClass);
+        }
+
       });
     });
 
