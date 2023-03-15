cc6a03042e10a82bbbde103da8ff3b882a434877
tzjg7293 <tzjg7293@gmail.com>
2023-03-15 15:48:55 -0700

    timeline small formats - colour

--- js/timeline.js
+++ js/timeline.js
@@ -3,7 +3,7 @@
     var selectors = {
       id: $(this),
       activeClass: "timeline-item--active",
-      activeDate: "timeline-date--active",
+      activeDate: "timeline-info--active",
       img: ".timeline__img"
     };
 
@@ -18,21 +18,16 @@
       $(".timeline-item").each(function (i, item) {
         content = $('.timeline-content');
         date = $('.timeline-info');
-
         min = $(this).offset().top;
         max = ($(this).height() + min);
         var item_middle = min + (max - min) / 2;
-        console.log("min: " + min);
-        console.log("max: " + max);
 
         if (i == itemLength - 2 && (pos + screen_height / 3) > item_middle) {
-          console.log("first");
           content.last().addClass(selectors.activeClass);
           date.last().addClass(selectors.activeDate);
           // selectors.item.removeClass(selectors.activeClass);
           // selectors.id.css("background-image", "url(" + selectors.item.last().find(selectors.img).attr('src') + ")"); //not sure what this is for, but it removes the after-lining of the box after transition
         } else if ((pos + screen_height / 3) <= max - 40 && (pos + screen_height / 3) >= min) {
-          console.log("this: " + (this));
           content.eq(i).addClass(selectors.activeClass);
           date.eq(i).addClass(selectors.activeDate);
           selectors.id.css("background-image", "url(" + $(this).find(selectors.img).attr('src') + ")");
