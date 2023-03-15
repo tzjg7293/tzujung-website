a0fd6695920cf6363b52a3ba8f93ad43bb056e3c
tzjg7293 <tzjg7293@gmail.com>
2023-03-15 15:23:10 -0700

    cleaned up timeline js

--- js/timeline.js
+++ js/timeline.js
@@ -7,28 +7,14 @@
       img: ".timeline__img"
     };
 
-    var all_item = $('.timeline-item');
-    // var all_item = document.getElementsByClassName('timeline-item');
-    // var all_item = document.querySelectorAll(".timeline-item *")
-    // var content = document.getElementsByClassName('timeline-content');
-    // var date = document.getElementsByClassName('timeline-info');
-
-    var itemLength = all_item.length;
-    console.log(itemLength);
-    console.log(all_item);
-
     $(window).scroll(function () {
       var max, min;
       var screen_height = screen.height;
       var pos = $(this).scrollTop();
       console.log("Pos: " + pos);
       $(".timeline-item").each(function (i, item) {
-        // console.log("i: " + i);
-        // console.log("item: " + item[i].outerHTML());
         content = $('.timeline-content');
         date = $('.timeline-info');
-        // console.log("content: " + content.text());
-        // console.log("date: " + date.text());
 
         min = $(this).offset().top;
         max = ($(this).height() + min);
@@ -50,30 +36,6 @@
           // selectors.item.removeClass(selectors.activeClass);
         }
       });
-
-      // for (var i = 0; i < itemLength; i++) {
-      //   // console.log("item: " + all_item.children[0]);
-      //   // console.log("content: " + content.text());
-      //   // console.log("date: " + date.text());
-
-      //   min = all_item[i].offset().top;
-      //   max = (all_item[i].height() + all_item[i].offset().top);
-      //   var item_middle = min + (max - min) / 2;
-      //   console.log("min: " + min);
-      //   console.log("max: " + max);
-
-      //   if (i == itemLength - 2 && (pos + screen_height/3) > item_middle) {
-      //     selectors.item.last().addClass(selectors.activeClass);
-      //     selectors.date.last().addClass(selectors.activeDate);
-      //     // selectors.item.removeClass(selectors.activeClass);
-      //     selectors.id.css("background-image", "url(" + selectors.item.last().find(selectors.img).attr('src') + ")"); //not sure what this is for, but it removes the after-lining of the box after transition
-      //   } else if ((pos + screen_height/3) <= max - 40 && (pos + screen_height/3) >= min) {
-      //     $(this).addClass(selectors.activeClass);
-      //     selectors.date.addClass(selectors.activeDate);
-      //     selectors.id.css("background-image", "url(" + $(this).find(selectors.img).attr('src') + ")");
-      //     // selectors.item.removeClass(selectors.activeClass);
-      //   }
-      // }
     });
   }
 })(jQuery);
