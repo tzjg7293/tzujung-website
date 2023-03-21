1870775725627c0f2d830214eb093df4a0e1b225
tzjg7293 <sydney.tj.fang@gmail.com>
2023-03-21 00:44:27 -0700

    created dataset class

--- chatbot/train.py
+++ chatbot/train.py
@@ -3,6 +3,9 @@ import json
 # import the functions that we created in nltk_utils.py
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('chatbot/intents.json', 'r') as f:
     intents = json.load(f)
@@ -44,5 +47,26 @@ for(w, tag) in xy:
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
+## Create a pytorch dataset from the x_train and y_train dataset
+class ChatDataset(Dataset): # Create a new class called chatDataset that inherits Dataset
 
+    # Constructor
+    def __init__(self):
+        self.n_samples = len(x_train) # Number of samples = length of x training data
+        self.x_data = x_train  
+        self.y_data = y_train
 
+    # Define the get item function so we can later on access dataset[idx]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index] # Return as a tuple of x and y data
+    
+    # Length method
+    def __len__(self):
+        return self.n_samples
+    
+    # Hyperparameters
+    batch_size = 8
+
+     
+    dataset = ChatDataset()
+    train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2) # Depending on num_workers, the more the faster it loads but takes more cpu
